b23f8b4598de6dcab0a051e85b8bb2d3ec019563
OKrawi <Krawi.ossama@gmail.com>
2021-05-05 04:56:38 +0300

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
-export const keys = {
-    prodURI: 'https://sa.aqar.fm/graphql',
-    devURI: 'https://dev-v3.aqar.fm/graphql'
+export default {
+    prodURI: "https://sa.aqar.fm/graphql",
+    devURI: "https://dev-v3.aqar.fm/graphql"
 }
