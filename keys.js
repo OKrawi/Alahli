509efc260ce05dac60303d3388585f22a1cab93b
OKrawi <Krawi.ossama@gmail.com>
2022-01-24 22:01:16 +0300

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 export default {
     prodURI: "https://sa.aqar.fm/graphql",
     devURI: "https://dev-v3.aqar.fm/graphql"
-<<<<<<< HEAD
+//<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 2cb4fe64f2652711cd935e3d69bc9aec556bf593
+//=======
+//}
+//>>>>>>> 2cb4fe64f2652711cd935e3d69bc9aec556bf593
